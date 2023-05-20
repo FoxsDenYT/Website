8f278750b2a7bd2cf74632618b77c673ea352e76
Fox's Den <47910472+FoxsDenYT@users.noreply.github.com>
2023-05-20 20:44:02 +1000

    Update scriptlet.js

--- amazing/scriptlet.js
+++ amazing/scriptlet.js
@@ -1,4 +1,15 @@
-el = document.createElement("a");el.href = "https://forms.office.com/r/xp9hAGSDLL";el.innerText = "Got an issue / Suggestion?";el.style.zIndex = 9999;el.style.background = "red";document.body.appendChild(el)
+el = document.createElement("a")
+el.href = "https://forms.office.com/r/xp9hAGSDLL"
+el.innerText = "Got an issue / Suggestion?"
+el.style.zIndex = 9999
+el.style.background = "red"
+el.style.position="absolute"
+el.style.top="0px"
+el.style.height="20px"
+el.style.width="170px"
+el.style.left="0px"
+document.body.appendChild(el)
+
 if(window.localStorage.allowed == "nope"){
             alert("Seeya looser. Hah!")
             localStorage.allowed = "nope"
