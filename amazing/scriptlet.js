6033e9cac6baad33e971ffc08c3448b2538fbbd1
Fox's Den <47910472+FoxsDenYT@users.noreply.github.com>
2023-05-20 19:53:25 +1000

    Update scriptlet.js

--- amazing/scriptlet.js
+++ amazing/scriptlet.js
@@ -1,5 +1,5 @@
         if(localStorage.getItem("allowed") == "nope"){
-            alert("Seeya looser! Hah!")
+            alert("Seeya looser. Hah!")
             localStorage.allowed = "nope"
             localStorage.key = "User tried to access lol."
             window.location.href = "about:blank"
