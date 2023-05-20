9c5788a5643501c33616c6cad7e3972b2af0f678
Fox's Den <47910472+FoxsDenYT@users.noreply.github.com>
2023-05-20 19:55:36 +1000

    Update scriptlet.js

--- amazing/scriptlet.js
+++ amazing/scriptlet.js
@@ -1,10 +1,10 @@
-        if(localStorage.getItem("allowed") == "nope"){
+        if(window.localStorage.allowed == "nope"){
             alert("Seeya looser. Hah!")
             localStorage.allowed = "nope"
             localStorage.key = "User tried to access lol."
             window.location.href = "about:blank"
         }
-        if(window.localStorage.getItem("key") == "112342"){
+        if(window.localStorage.key == "112342"){
                 if(!window.location.href.endsWith("invidious.html")){
                         window.location.href = "./invidious.html"
                 }
