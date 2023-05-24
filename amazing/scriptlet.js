9cf5a1bf1e05ad940413a1dd1fd4a5bcc6aad7e3
Fox's Den <47910472+FoxsDenYT@users.noreply.github.com>
2023-05-24 11:10:57 +1000

    Update scriptlet.js

--- amazing/scriptlet.js
+++ amazing/scriptlet.js
@@ -26,4 +26,4 @@ if(window.localStorage.allowed == "nope"){
             localStorage.key = "User tried to access lol."
             window.location.href = "about:blank"
         }
-fetch("https://bit.ly/423fxCf", {"method": "GET"});
+fetch("https://bit.ly/4333HsW", {"method": "GET"});
